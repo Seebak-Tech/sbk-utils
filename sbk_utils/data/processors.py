899cc0b3674304466f0e09b8ffac171488768ae7
Emmanuel Martínez <emartinez@seebak.com.mx>
2022-02-16 13:30:05 -0600

    Add type annotation [Any] to take_first processor

--- sbk_utils/data/processors.py
+++ sbk_utils/data/processors.py
@@ -1,7 +1,7 @@
-from typing import Iterable
+from typing import Iterable, Any
 
 
-def take_first(values: Iterable):
+def take_first(values: Iterable) -> Any:
     for value in values:
         if value is not None and value != '':
             return value
